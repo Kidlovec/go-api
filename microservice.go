72bd5a3593abf5aed4eb558f87289fa422d9d126
wenjie.fan <fanwenjie415@pingan.com.cn>
2019-03-16 09:23:24 +0800

    echo methods

--- microservice.go
+++ microservice.go
@@ -8,8 +8,8 @@ import (
 
 func main()  {
   http.HandleFunc("/", index)
+  http.HandleFunc("/api/echo", echo )
   http.ListenAndServe(port(), nil)
-
 }
 
 func port() string {
@@ -23,4 +23,11 @@ func port() string {
 func index(w http.ResponseWriter, r *http.Request)  {
   w.WriteHeader(http.StatusOK)
   fmt.Fprintf(w, "Hello Cloud Native Go. ")
+}
+
+func echo(w http.ResponseWriter, r *http.Request)  {
+  message := r.URL.Query()["message"][0]
+
+  w.Header().Add("Content-Type", "text/plain")
+  fmt.Fprintf(w, message)
 }
