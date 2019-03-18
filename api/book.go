6c8114f2583a39ef4028a89a2048a9090b1b4949
wenjie.fan <fanwenjie415@pingan.com.cn>
2019-03-18 16:28:44 +0800

    x

--- api/book.go
+++ api/book.go
@@ -1,60 +1,178 @@
 package api
 
 import (
-  "encoding/json"
-  "net/http"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
 
-  "fmt"
+	"fmt"
 )
 
 // Book type with Name, Author and ISBN
 type Book struct {
-  Title string `json:"title"`
-  Author string `json:"author"`
-  ISBN string `json:"isbn"`
+	Title       string `json:"title"`
+	Author      string `json:"author"`
+	ISBN        string `json:"isbn"`
+	Description string `json:"description,omitempty"`
 
-  // define the book
+	// define the book
 }
 
 // ToJSON to be used for marshalling of Book type
-func (b Book) ToJSON() [] byte {
-  ToJSON, err := json.Marshal(b);
-  if err != nil {
-    panic(err)
-  }
-  return ToJSON
+func (b Book) ToJSON() []byte {
+	ToJSON, err := json.Marshal(b)
+	if err != nil {
+		panic(err)
+	}
+	return ToJSON
+}
+
+// Books slice of all known books
+var books = map[string]Book{
+	"0345391802": Book{Title: "The Hitchhiker's Guide to the Galaxy", Author: "Douglas Adams", ISBN: "0345391802"},
+	"0123456789": Book{Title: "Cloud Native Go", Author: "M.-L. Reimer", ISBN: "0123456789"},
 }
 
 // FromJSON to be used for unmarshalling of Book type
 func FromJSON(data []byte) Book {
-  book := Book{}
-  err := json.Unmarshal(data, &book)
-  if err != nil {
-    panic(err)
-  }
-  return book
+	book := Book{}
+	err := json.Unmarshal(data, &book)
+	if err != nil {
+		panic(err)
+	}
+	return book
 }
 
+// WriteJSON writes response to the wirter base on the interface
+func WriteJSON(w http.ResponseWriter, i interface{}) {
+	b, err := json.Marshal(i)
+	if err != nil {
+		panic(err)
+	}
 
-func EchoHandleFunc(w http.ResponseWriter, r *http.Request)  {
-  message := r.URL.Query()["message"][0]
-
-  w.Header().Add("Content-Type", "text/plain")
-  fmt.Fprintf(w, message)
+	w.Header().Add("Content-Type", "application/json; charset=utf-8")
+	w.Write(b)
 }
 
-// Books slice of all known books
-var Books = []Book{
-  Book{Title:"The Hitchhiker's Guide to the Galaxy", Author: "Douglas Adams", ISBN: "0345391802"},
-  Book{Title: "Cloud Native Go", Author: "M.-L. Reimer", ISBN: "0123456789"},
+// EchoHandleFunc echos what passed from http client
+func EchoHandleFunc(w http.ResponseWriter, r *http.Request) {
+	message := r.URL.Query()["message"][0]
+
+	w.Header().Add("Content-Type", "text/plain")
+	fmt.Fprintf(w, message)
 }
+
 // BooksHandleFunc to be used as http.HandleFunc for Book API
-func BooksHandleFunc(w http.ResponseWriter, r *http.Request) {
-  b, err := json.Marshal(Books)
-  if err != nil{
-    panic(err)
-  }
-  w.Header().Add("Content-Type", "application/json; charset=utf-8")
+func BookHandleFunc(w http.ResponseWriter, r *http.Request) {
+	isbn := r.URL.Path[len("/api/books/"):]
 
-  w.Write(b)
+	switch method := r.Method; method {
+	case http.MethodGet:
+		book, found := GetBook(isbn)
+		if found {
+			WriteJSON(w, book)
+		} else {
+			w.WriteHeader(http.StatusNotFound)
+		}
+	case http.MethodPut:
+		body, err := ioutil.ReadAll(r.Body)
+
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+
+		book := FromJSON(body)
+		exists := UpdateBook(isbn, book)
+		if exists {
+			w.WriteHeader(http.StatusOK)
+		} else {
+			w.WriteHeader(http.StatusNotFound)
+		}
+
+	case http.MethodDelete:
+		DeleteBook(isbn)
+		w.WriteHeader(http.StatusOK)
+
+	default:
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("Unsupported request method."))
+
+	}
+
+}
+
+// BookHandleFunc to be used as http.HandleFunc for Book API
+func BooksHandleFunc(w http.ResponseWriter, r *http.Request) {
+	switch method := r.Method; method {
+	case http.MethodGet:
+		books := GetAllBooks()
+		WriteJSON(w, books)
+	case http.MethodPost:
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+
+		book := FromJSON(body)
+		isbn, created := CreateBook(book)
+
+		if created {
+			w.Header().Add("Location", "/api/books/"+isbn)
+			w.WriteHeader(http.StatusCreated)
+		} else {
+			w.WriteHeader(http.StatusConflict)
+		}
+
+	default:
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("Unsupported request method."))
+	}
+
+}
+
+// GetAllBooks return a slice of all books
+func GetAllBooks() []Book {
+	values := make([]Book, len(books))
+	index := 0
+	for _, book := range books {
+		values[index] = book
+		index++
+	}
+
+	return values
+}
+
+// GetBook return the book for a given ISBN
+func GetBook(isbn string) (Book, bool) {
+	book, found := books[isbn]
+	return book, found
+}
+
+// CreateBook creates a new book if it does not exist
+func CreateBook(book Book) (string, bool) {
+	_, exists := books[book.ISBN]
+
+	if exists {
+		return "", false
+	}
+
+	books[book.ISBN] = book
+	return book.ISBN, true
+}
+
+// UpdateBook updates an existing book
+func UpdateBook(isbn string, book Book) bool {
+
+	_, exists := books[isbn]
+
+	if exists {
+		books[isbn] = book
+	}
+
+	return exists
+}
+
+// DeleteBook removes a book from the map by ISBN key
+func DeleteBook(isbn string) {
+	delete(books, isbn)
 }
--- microservice.go
+++ microservice.go
@@ -1,29 +1,30 @@
 package main
 
 import (
-  "fmt"
-  "net/http"
-  "os"
+	"fmt"
+	"net/http"
+	"os"
 
-  "./api"
+	"./api"
 )
 
-func main()  {
-  http.HandleFunc("/", index)
-  http.HandleFunc("/api/books", api.BooksHandleFunc)
-  http.HandleFunc("/api/echo", api.EchoHandleFunc )
-  http.ListenAndServe(port(), nil)
+func main() {
+	http.HandleFunc("/", index)
+	http.HandleFunc("/api/books", api.BooksHandleFunc)
+	http.HandleFunc("/api/books/", api.BookHandleFunc)
+	http.HandleFunc("/api/echo", api.EchoHandleFunc)
+	http.ListenAndServe(port(), nil)
 }
 
 func port() string {
-  port := os.Getenv("PORT")
-  if len(port) == 0 {
-    port = "8080"
-  }
-  return ":" + port
+	port := os.Getenv("PORT")
+	if len(port) == 0 {
+		port = "8080"
+	}
+	return ":" + port
 }
 
-func index(w http.ResponseWriter, r *http.Request)  {
-  w.WriteHeader(http.StatusOK)
-  fmt.Fprintf(w, "Hello Cloud Native Go. ")
+func index(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Hello Cloud Native Go. ")
 }
